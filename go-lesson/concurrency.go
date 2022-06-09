0754e0974b941e75baf226cdaf10403d9d97f7c7
Nhan Tran <nhan.tran@s3corp.com.vn>
2022-06-09 17:36:56 +0700

    feat: implement multiple goroutines

--- go-lesson/concurrency.go
+++ go-lesson/concurrency.go
@@ -3,8 +3,11 @@ package golesson
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func Concurrency(animal string) {
 	for i := 0; i <= 5; i++ {
@@ -12,21 +15,20 @@ func Concurrency(animal string) {
 		time.Sleep(time.Millisecond * 400)
 	}
 }
-func middleWareChannel(c chan string, arr []string) {
-	for i := 0; i < len(arr); i++ {
-		strToNum, err := strconv.Atoi(arr[i])
-		if err != nil {
-			c <- arr[i]
-		} else {
-			numCombine := ""
-			for i := 0; i <= strToNum; i++ {
-				intToStr := strconv.Itoa(i)
-				numCombine += intToStr
-			}
-			c <- numCombine
+
+func middleWareChannel(c chan string, arr string) {
+	defer wg.Done()
+	strToNum, err := strconv.Atoi(arr)
+	if err != nil {
+		c <- arr
+	} else {
+		numCombine := ""
+		for i := 0; i <= strToNum; i++ {
+			intToStr := strconv.Itoa(i)
+			numCombine += intToStr
 		}
+		c <- numCombine
 	}
-	close(c)
 }
 
 func DeadLockChecker() {
@@ -35,8 +37,26 @@ func DeadLockChecker() {
 	arrayValues := []string{"Nguyen Huu Phuc", "5", "Phuc", "24", "22"}
 	chanDeadLookCreation := make(chan string, len(arrayValues))
 
-	go middleWareChannel(chanDeadLookCreation, arrayValues)
-	for i := range chanDeadLookCreation {
-		fmt.Println(i)
+	for _, value := range arrayValues {
+		wg.Add(1)
+		go middleWareChannel(chanDeadLookCreation, value)
 	}
+
+	for i := 0; i < len(arrayValues); i++ {
+		select {
+		case value, ok := <-chanDeadLookCreation:
+			if !ok {
+				chanDeadLookCreation = nil
+			}
+			fmt.Println(value)
+		}
+		if chanDeadLookCreation == nil {
+			break
+		}
+	}
+
+	go func() {
+		defer close(chanDeadLookCreation)
+		wg.Wait()
+	}()
 }
